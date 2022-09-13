7844e6e5c90bd2151c942c23d34a81300ff2c3a4
Ezequiel <arangue@users.noreply.github.com>
2022-09-13 17:20:32 -0300

    Bundle enums into a separate folder (#2)
    * Add temperature controller and service
    
    * Changes

--- src/main/java/com/converter/unit/controller/LengthController.java
+++ src/main/java/com/converter/unit/controller/LengthController.java
@@ -1,8 +1,8 @@
 package com.converter.unit.controller;
 
+import com.converter.unit.domain.enums.LengthUnit;
 import com.converter.unit.exception.InvalidUnitException;
 import com.converter.unit.service.LengthService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,13 +19,9 @@ public class LengthController {
         this.service = service;
     }
 
-    public enum units {
-        mile, kilometer, yard, meter, centimeter, foot, inch, millimeter
-    }
-
     @GetMapping("/length")
     public ResponseEntity<?> length() {
-        return ResponseEntity.ok().body(units.values());
+        return ResponseEntity.ok().body(LengthUnit.values());
     }
 
     @GetMapping("/length/{unit}")
---
+++ src/main/java/com/converter/unit/domain/enums/LengthUnit.java
@@ -0,0 +1,5 @@
+package com.converter.unit.domain.enums;
+
+public enum LengthUnit {
+    centimeter, foot, inch, kilometer, meter, mile, millimeter, yard
+}
